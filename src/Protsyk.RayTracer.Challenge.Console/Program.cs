848293f3b591184e8bb49de3ea588fa97474799b
petro <petro@unknown.com>
2019-09-14 10:06:12 +0200

    Project Template

---
+++ src/Protsyk.RayTracer.Challenge.Console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Protsyk.RayTracer.Challenge.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
